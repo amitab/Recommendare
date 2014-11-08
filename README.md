f00866ddcd70506501128640df1c501a37318b58
Amitabh Das <amitab.das@outlook.com>
2014-11-08 16:00:32 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Recommendare
+A python based hybrid recommendation system built from scratch
+
+## Set it up:
+1. Download the movie lens dataset from http://www.grouplens.org/datasets/movielens/
+2. Extract it and pass the directory path + the extracted folder to the extracted directory to extractor.py in the data folder
+3. 3 files are generated: movies.json, users.json, meta.json
+4. Import to mongodb and start
---
+++ lib/SimilarityMeasure.py
@@ -0,0 +1,33 @@
+import math
+
+def SimilarityMeasure:
+    
+    @classmethod
+    def cosine_similarity(vector_x, vector_y):
+        dot_pdt = 0
+        length_x = 0
+        length_y = 0
+
+        for i in range(len(vector_x)):
+            dot_pdt += vector_x[i] * vector_y[i]
+            length_x += vector_x[i] ** 2
+            length_y += vector_y[i] ** 2
+
+        length_x = math.sqrt(length_x)
+        length_y = math.sqrt(length_y)
+        den = length_x * length_y
+
+        if den == 0:
+            return 0
+        else:
+            return dot_pdt / float(den)
+    
+    @classmethod
+    def deviation(vector_x, vector_y, card_x_y):
+        dev = 0
+
+        for i in range(len(vector_x))
+            num = vector_x[i] - vector_y[i]
+            dev += num / float(card_x_y)
+
+        return dev
